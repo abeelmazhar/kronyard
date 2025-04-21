0ed962f35a5b27e2814d16a02fb6631d259e9801
AbeelMazhar <abeelmazhar4@gmail.com>
2025-04-22 00:21:13 +0500

    layout set

--- app/(tabs)/products.tsx
+++ app/(tabs)/products.tsx
@@ -3,8 +3,25 @@ import React from 'react'
 import className from 'twrnc'
 const products = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>products</Text>
+    <View style={className`flex-1 items-center justify-center mt-10`}>
+    <View style={[className`bg-blue-500 w-full h-1/3 `]}>
+    <Text style={className`text-white text-2xl`}>
+        
+    </Text>
+    </View> 
+    
+     <View style={className`bg-green-500  w-full h-1/3`}>
+<Text style={className`text-white text-2xl`}>Products</Text>
+    </View>
+
+    <View style={className`bg-yellow-500  w-full h-1/6`}>
+    <Text style={className`text-white text-2xl`}>Products</Text>
+    </View>
+
+    <View style={className`bg-orange-500  w-full h-1/6`}>
+    <Text style={className`text-white text-2xl`}>Products</Text>
+        </View>   
+    
     </View>
   )
 }
