895d094916200fa3a332de61b55a668478d8e62f
AbeelMazhar <abeelmazhar4@gmail.com>
2025-04-22 00:36:21 +0500

    image addded

--- app/(tabs)/products.tsx
+++ app/(tabs)/products.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import className from 'twrnc'
+
 const products = () => {
   return (
     <View style={className`flex-1 items-center justify-center mt-10`}>
-    <View style={[className`bg-blue-500 w-full h-1/3 `]}>
-    <Text style={className`text-white text-2xl`}>
-        
-    </Text>
-    </View> 
+  <View style={className`w-full h-1/3 bg-white`}>
+     
+        <Image
+          source={{ uri: 'https://images.unsplash.com/photo-1583121274602-3e2820c69888?w=600' }}
+          style={className`w-full h-full opacity-70` }
+          resizeMode="cover"
+        />
+     
+    </View>
     
      <View style={className`bg-green-500  w-full h-1/3`}>
 <Text style={className`text-white text-2xl`}>Products</Text>
