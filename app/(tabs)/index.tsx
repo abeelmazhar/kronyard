4259d0a463eba8d1b6fea7d9050c3b66486a4f7a
Abeel mazhar <abeelmazhar4@gmail.com>
2025-04-22 10:30:22 +0500

    testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,33 @@
-import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
-import className from 'twrnc'
-import HeroSection from '@/components/HeroSection'
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React from "react";
+import className from "twrnc";
+import HeroSection from "@/components/HeroSection";
 
 const Home = () => {
   return (
     <View style={className`flex-1 items-center justify-center mt-10`}>
-    <HeroSection />
-       
-        <View style={className`bg-green-500  w-full h-1/3`}>
-   <Text style={className`text-white text-2xl`}>Products</Text>
-       </View>
-   
-       <View style={className`bg-yellow-500  w-full h-1/6`}>
-       <Text style={className`text-white text-2xl`}>Products</Text>
-       </View>
-   
-       <View style={className`bg-orange-500  w-full h-1/6`}>
-       <Text style={className`text-white text-2xl`}>Products</Text>
-           </View>   
-       
-       </View>
-  )
-}
+      {/* <HeroSection /> */}
 
-export default Home
+      <View style={className`bg-green-500  w-full h-1/3`}>
+        <Text style={className`text-white text-2xl`}>Products</Text>
+      </View>
+
+      <View style={className`bg-yellow-500  w-full h-1/6`}>
+        <Text style={className`text-white text-2xl`}>Products</Text>
+      </View>
+
+      <View style={className`bg-orange-500  w-full h-1/6`}>
+        <Text style={className`text-white text-2xl`}>Products</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
