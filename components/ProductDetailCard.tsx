11c577f25d5d6e7373aca7b8d38d778b8f94d903
AbeelMazhar <abeelmazhar4@gmail.com>
2025-04-23 23:25:28 +0500

    feat: add sorting and filtering options to ProductDetailCard

--- components/ProductDetailCard.tsx
+++ components/ProductDetailCard.tsx
@@ -14,6 +14,16 @@ const ProductDetailCard = () => {
 
   return (
     <>
+    <View style={className`w-full h-16 bg-white shadow-md flex-row items-center justify-between px-4`}>
+    <Pressable  style={className`flex-row items-center justify-center bg-gray-300 rounded-3xl px-4 py-2 w-1/3 `}>
+  <MaterialIcons name="sort" size={24} color="black" />
+  <Text style={className`text-gray-500 text-base`}>Sort</Text>
+</Pressable>
+<Pressable  style={className`flex-row items-center bg-gray-300 rounded-3xl px-4 py-2 w-1/3 justify-center`}>
+  <MaterialIcons name="filter-list" size={24} color="black" />
+  <Text style={className`text-gray-500 text-base`}>Filter</Text>
+</Pressable>
+    </View>
       <ScrollView style={className`w-full px-2 mb-4`}>
         {dummyProducts.map((product) => (
           <View key={product.id} style={className`w-full h-auto gap-2 mb-8`}>
