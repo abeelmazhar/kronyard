ead0bf9e5b507d4f04b484c16c20b218979eb412
AbeelMazhar <abeelmazhar4@gmail.com>
2025-05-25 21:28:56 +0500

    refactor: uncomment extraReducers in userSlice to enable user fetching functionality

--- slice/userSlice.ts
+++ slice/userSlice.ts
@@ -14,19 +14,19 @@ const userSlice = createSlice({
     error: null,
   },
   reducers: {},
-  // extraReducers: (builder) => {
-  //   builder
-  //     .addCase(fetchUsers.pending, (state) => {
-  //       state.loading = true;
-  //     })
-  //     .addCase(fetchUsers.fulfilled, (state, action) => {
-  //       state.loading = false;
-  //       state.data = action.payload;
-  //     })
-  //     .addCase(fetchUsers.rejected, (state, action) => {
-  //       state.loading = false;
-  //       state.error = action.error.message || undefined;
-  //     });
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || undefined;
+      });
   },
 });
 
