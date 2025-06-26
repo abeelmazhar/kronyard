db409782a100d53cb11a489207996546ee3e48f9
Abeel mazhar <abeelmazhar4@gmail.com>
2025-06-26 14:32:19 +0500

    refactor: comment out DropDownSection and ProductSection components for future implementation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,8 +10,8 @@ const Home = () => {
   return (
     <View style={className` items-center justify-center `}>
       <HeroSection />
-      <DropDownSection />
-      <ProductSection />
+      {/* <DropDownSection />
+      <ProductSection /> */}
       <BottomSection />
     </View>
   );
